243e127b7f3cadbe64249f3f3dcde87597cedd5e
nQuery512 <nQuery512@gmail.com>
2017-02-05 04:22:29 +0100

    - Remove graphical bug
    Remove a conflict between AppTheme and the theme of the MainMenu

--- main/java/com/example/qyybaassh/r6tools/MainActivity.java
+++ main/java/com/example/qyybaassh/r6tools/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.qyybaassh.r6tools;
 
-import android.app.Application;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
